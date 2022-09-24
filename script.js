936b374e50c1484b507690a1468f9d639d0ddeb2
Lens <98659047+Lensco825@users.noreply.github.com>
2022-09-24 23:27:06 +0000

    This is a re-do of the change color commit since something went wrong and i had to re-do it

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-var first = document.getElementById("first");
-var second = document.getElementById("second");
-var third = document.getElementById("third");
-var fourth = document.getElementById("fourth");
-var fifth = document.getElementById("fifth");
-var parent = document.querySelector("div");
 
-var lists = [first, second, third, fourth, fifth];
+var setting = document.querySelector("ul");
 
-var text = document.createElement("p");
-parent.appendChild(text);
+function colorize() {
+  document.body.style.background = "#D9B8C4";
+}
 
-function show() {
-  text.innerHtml = lists.value;
+function reedem() {
+  document.body.style.background = "#FF1654";
 }
